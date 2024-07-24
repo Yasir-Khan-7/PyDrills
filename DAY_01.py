c7c45c626fd85240f5c7a5ab2029a794e405add5
Yasir-Khan-7 <yasirkhandps7@gmail.com>
2024-07-24 13:50:04 +0500

    Update DAY_01.py

--- DAY_01.py
+++ DAY_01.py
@@ -23,12 +23,31 @@ def mini_Calculator(x,y,operator):
             return x/y
         else:
             print("invalid Operator... ")
-    except  Exception as ValueError:
+    except ValueError:
+        print(ValueError)
         
         
 
 num_1 = input("Enter number 1: ")
 num_2 = input("Enter number 2: ")
 operator = input("Enter operator +,-,/,* to perform operation:  ")
-sum(num_1,num_2,operator)    
+mini_Calculator(int(num_1),int(num_2),operator)    
+
 # Q 3: Convert temperature from Celsius to Fahrenheit.
+def Temperature_conversion(Temp,type_of_conversion):
+    print(Temp)
+    if type_of_conversion =="F":
+        celsious = (Temp-32)*(5/9)
+        return round(celsious,2)
+    
+    elif type_of_conversion =="C":
+        Farhenheit = (Temp*9/5)+32
+        return round(Farhenheit,2)
+    
+Temp = input("Enter Temperature: ")
+type_of_conversion = input("Enter conversion type 'F' for Farhenheit and 'C' for Celsius:  ")
+
+result =Temperature_conversion(int(Temp),type_of_conversion)
+print(result)
+
+    
